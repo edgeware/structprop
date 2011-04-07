6630e94959e8fdf87de7b9dd34ca24442f728dac
Johan Rydberg <johan.rydberg@edgeware.tv>
2011-04-07 14:54:12 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='structprop',
-      version='0.0.2',
+      version='0.0.3',
       description='Parser for structured property config file format',
       author='Johan Rydberg',
       author_email='johan.rydberg@gmail.com',
