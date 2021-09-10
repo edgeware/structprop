549a35265465fb341c39d939cf27867c2a56df51
Peter Lithammer <peter.lithammer@edgeware.tv>
2021-09-10 16:35:16 +0200

    Release v0.0.11

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='structprop',
-      version='0.0.10',
+      version='0.0.11',
       description='Parser for structured property config file format',
       author='Edgeware AB',
       author_email='info@edgeware.tv',
